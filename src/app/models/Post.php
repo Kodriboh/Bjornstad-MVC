94f0f630279dc5d1d820694515759c762e6ed6a9
LukeMcCann <developer-lukemccann@outlook.com>
2020-11-22 15:45:27 +0000

    Add Post example

--- src/app/models/Post.php
+++ src/app/models/Post.php
@@ -11,4 +11,16 @@ class Post
     public function __construct() {
         $this->db = new Database;
     }
+
+    /**
+     * Retrieves all posts from the Database
+     *
+     * @return array[Object]
+     */
+    public function getPosts() 
+    {
+        $this->db->query('SELECT * FROM posts');
+
+        return $this->db->resultSet();
+    }
 }
